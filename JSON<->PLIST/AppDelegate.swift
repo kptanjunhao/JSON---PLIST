9e7ac33031c1aacaa6a798d860d38f97d8daf82b
kptanjunhao <244296129@qq.com>
2016-06-23 15:24:07 +0800

    Initial Commit

---
+++ JSON<->PLIST/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSON<->PLIST
+//
+//  Created by 谭钧豪 on 16/6/23.
+//  Copyright © 2016年 谭钧豪. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JSON<->PLIST/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSON<->PLIST
+//
+//  Created by 谭钧豪 on 16/6/23.
+//  Copyright © 2016年 谭钧豪. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
